d7d6218602ac1ec1f2c8e783b3079853a0eec21d
Eike Decker <zet23t@googlemail.com>
2015-04-17 13:44:39 +0200

    Some work in progress code

--- image.h
+++ image.h
@@ -35,3 +35,21 @@ static void ImageInclude_readLineInto(const ImageInclude *img, unsigned char *li
 		}
 	}
 }
+/*
+Tile set related image drawing. Right now not important for me. Implement later.
+
+static void ImageInclude_readTileMapInto(const ImageInclude *img, unsigned char *lineBuffer,
+	unsigned char offsetY, unsigned char offsetX, unsigned char mapX, unsigned char mapY,
+	unsigned char uvShiftX, unsigned char uvShiftY, unsigned char *uvOffsetMap, unsigned char mapWidth)
+{
+	unsigned char width = pgm_read_byte(&img->width);
+	unsigned int data = pgm_read_word(&img->data);
+	
+	unsigned char x = 0;
+	unsigned char step = 1<<uvShiftX;
+	while (x<96) {
+		unsigned int offset = data + offsetY * width;
+		x+=step;
+	}
+
+}*/
